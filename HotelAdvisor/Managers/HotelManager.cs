94bc6858cf4efa6a35f49381c37e0227e239d0e1
n.babin <n.babin@levi9.com>
2015-05-07 22:42:44 +0200

    Inital commit

---
+++ HotelAdvisor/Managers/HotelManager.cs
@@ -0,0 +1,21 @@
+﻿using HotelAdvisor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelAdvisor.Managers
+{
+    public class HotelManager
+    {
+        public void Create(Hotel hotel)
+        {
+            using (var context = new HotelAdvisorContext())
+            {
+                context.Hotels.Add(hotel);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ HotelAdvisor/Models/Comment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HotelAdvisor.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int HotelId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Text { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+
+        [ForeignKey("HotelId")]
+        public virtual Hotel Hotel { get; set; }
+    }
+}
